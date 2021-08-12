db5b8c2a40c5dfdc16fa607d951b5da6e92f17fc
”yk2001525“ <1272689242@qq.com>
2021-08-12 13:46:31 +0800

    08月12日13:46:28

--- controller/product.js
+++ controller/product.js
@@ -10,12 +10,47 @@ const SearchProduct =(keyword)=>{
     return exec(sql)
 }
 const addShopCart = (user_id,product_id)=>{
-    let sql = `insert into shopcart (user_id,product_id,num) VALUES ('${user_id}','${product_id}','1')`
+    let sql = `insert into shopcart (user_id,product_id,num,checked) VALUES ('${user_id}','${product_id}','1','1')`
+    return exec(sql)
+}
+const checkShopCart = (user_id,product_id)=>{
+    let sql = `select * from shopcart where user_id = '${user_id}' and product_id = '${product_id}'`
+    return exec(sql)
+}
+const addShopNum = (user_id,product_id)=>{
+    let sql = `update shopcart set num = num+1 where user_id = '${user_id}' and product_id = '${product_id}'`
+    return exec(sql)
+}
+const getMyShopCart=(user_id)=>{
+    let sql = `select * from product,shopcart WHERE product.product_id = shopcart.product_id and shopcart.user_id = '${user_id}'`
+    return exec(sql)
+}
+const deleteShopCart=(user_id,product_id)=>{
+    let sql = `delete from shopcart where user_id = '${user_id}' and product_id = '${product_id}'`
     return exec(sql)
 }
 
+const checkProduct = (user_id,product_id)=>{
+    let sql = `update shopcart set checked = !checked where user_id = '${user_id}' and product_id = '${product_id}'`
+    return exec(sql)
+}
+const allCheck = (user_id)=>{
+    let sql = `update shopcart set checked = 1 where user_id = '${user_id}'`
+    return exec(sql)
+}
+const allNoCheck = (user_id)=>{
+    let sql = `update shopcart set checked = 0 where user_id = '${user_id}'`
+    return exec(sql)
+}
 module.exports={
     GetProduct,
     SearchProduct,
-    addShopCart
+    addShopCart,
+    checkShopCart,
+    addShopNum,
+    getMyShopCart,
+    deleteShopCart,
+    checkProduct,
+    allCheck,
+    allNoCheck
 }
--- routes/product.js
+++ routes/product.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 const { SuccessModel, ErrorModel} = require('../model/resModel')
-const {SearchProduct,GetProduct,addShopCart} = require('../controller/product')
+const {SearchProduct,GetProduct,addShopCart,checkShopCart,addShopNum,getMyShopCart,deleteShopCart,checkProduct,allCheck,allNoCheck} = require('../controller/product')
 
 router.post('/getproduct',function(req,res,next){
     const result = GetProduct()
@@ -33,5 +33,72 @@ router.post('/addshopcart',function(req,res,next){
     })
 })
 
+router.post('/checkshopcart',function(req,res,next){
+    const {user_id,product_id} = req.body
+    const result = checkShopCart(user_id,product_id)
+    return result.then((List)=>{
+        res.json(
+            new SuccessModel(List) 
+        )
+    })
+})
 
+router.post('/addshopcartnum',function(req,res,next){
+    const {user_id,product_id}=req.body
+    const result = addShopNum(user_id,product_id)
+    return result.then((List)=>{
+        res.json(
+            new SuccessModel(List)
+        )
+    })
+
+})
+router.post('/getshopcart',function(req,res,next){
+    const{user_id}=req.body
+    const result = getMyShopCart(user_id)
+    return result.then((List)=>{
+        res.json(
+            new SuccessModel(List)
+        )
+    })
+
+})
+router.post('/deleteshopcart',function(req,res,next){
+    const {user_id,product_id} = req.body
+    const result = deleteShopCart(user_id,product_id)
+    const results = getMyShopCart(user_id)
+    return results.then((List)=>{
+        res.json(
+            new SuccessModel(List)
+        )
+    })
+})
+router.post('/checkproduct',function(req,res,next){
+    const{user_id,product_id} = req.body
+    const result = checkProduct(user_id,product_id)
+    return result.then((Results)=>{
+        res.json(
+            new SuccessModel(Results)
+        )
+    })
+
+})
+router.post('/allcheck',function(req,res,next){
+    const {user_id} = req.body
+    const result = allCheck(user_id)
+    return result.then((Results)=>{
+        res.json(
+            new SuccessModel(Results)
+        )
+    })
+})
+router.post('/allnocheck',function(req,res,next){
+    const {user_id} = req.body
+    const result = allNoCheck(user_id)
+    return result.then((Results)=>{
+        res.json(
+            new SuccessModel(Results)
+        )
+    })
+})
 module.exports = router;
